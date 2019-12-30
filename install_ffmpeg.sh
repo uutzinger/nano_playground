6de18242e030990c7f683097a27e1c009dcdc2b4
Urs Utzinger <uutzinger@gmail.com>
2019-12-30 09:55:08 -0700

    ffmpeg

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -9,6 +9,10 @@ make
 sudo make install
 sudo ldconfig
 
+# dependencies
+apt install libsdl2-dev # for ffplay
+
+
 git clone git://source.ffmpeg.org/ffmpeg.git -b release/4.2 --depth=1
 cd ffmpeg
 wget https://github.com/jocover/jetson-ffmpeg/raw/master/ffmpeg_nvmpi.patch
@@ -17,8 +21,11 @@ git apply ffmpeg_nvmpi.patch
 make -j4
 sudo make install
 
+export RTSP_PATH=rtsp://192.168.11.202:8554/unicast
+gst-launch-1.0 rtspsrc location="$RTSP_PATH" latency=100 ! rtph264depay ! h264parse ! omxh264dec ! nvoverlaysink overlay-x=0 overlay-y=20 overlay-w=960 overlay-h=540 overlay=2
+
 # test on network camera
-ffplay -max_delay 500000 -rtsp_transport udp rtsp://192.168.8.52:8554/unicast
+ffplay -max_delay 500000 -rtsp_transport udp "$RTSP_PATH"
 # core dump dbus
 
 gst-launch-1.0 videotestsrc is-live=true ! video/x-raw, width=1920, height=1080  ! nvoverlaysink
